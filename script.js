0ab15df603f079ac74660355c0a15348a1816383
Jothin Kumar <contact@jothin.tech>
2024-05-02 23:20:30 +0530

    animation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const coinContainer = document.getElementById("coin-container")
 window.side1 = document.getElementById("coin-head")
 window.side2 = document.getElementById("coin-tail")
 
@@ -12,6 +13,12 @@ function flipCoin() {
             side.classList.add("coin-side-1")
         }
     })
+    coinContainer.style.animation = "coin-flip 2s forwards"
+    document.body.classList.add("coin-flip")
+    setTimeout(() => {
+        coinContainer.style.animation = ""
+        document.body.classList.remove("coin-flip")
+    }, 2000)
 }
 
 document.getElementById("coin-container").addEventListener("click", () => {
